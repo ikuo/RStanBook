26dbc21dd693de16518edceec6b9e44f5af55747
Ikuo Matsumura <ikuo@initials.im>
2018-02-25 16:42:16 +0900

    Add Sys.time()

--- chap04/exercise/ex5.R
+++ chap04/exercise/ex5.R
@@ -1,9 +1,11 @@
 library(rstan)
-
 source('generate-data.R')
 
 data <- list(N1=N1, N2=N2, Y1=Y1, Y2=Y2)
+t1 <- Sys.time()
 fit <- stan(file='ex5.stan', data=data, seed=1234)
+t2 <- Sys.time()
+t2 - t1
 
 ms <- extract(fit)
 prob <- mean(ms$mu1 < ms$mu2)  #=> 0.9457
